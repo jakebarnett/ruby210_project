d3b08684b04e9d29bee9bcd4dc76b181e6db8aae
Jake Barnett <mrjbarnett@gmail.com>
2016-02-17 07:31:44 +0000

    READ operation, show single project feature and spec

---
+++ spec/features/project/show_project_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.feature "shows a single project" do
+    before(:all) do
+        @project1 = FactoryGirl.create(:project)
+    end
+    
+    scenario "it displays the info for a project" do
+        visit '/projects'
+        
+        click_link "Project #{@project1.name} Info"
+        
+        expect(page).to have_content(@project1.name)
+        expect(page).to have_content(@project1.description)
+    end
+end
